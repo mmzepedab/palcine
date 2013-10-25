c4525a94664721f5ebebc06a8adb5125600a30df
mmzepedab <mmzepedab@Mario-Zepedas-MacBook-Pro.local>
2013-10-25 08:03:02 -0600

    Continue Creating database structure

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,14 +47,25 @@ return array(
 			),
 		),
 		*/
+		 //MySQL database on PC Server
+//                'db'=>array(
+//                            'connectionString' => 'mysql:host=localhost;dbname=palcineweb',
+//                            'emulatePrepare' => true,
+//                            'username' => 'root',
+//                            'password' => '',
+//                            'charset' => 'utf8',
+//                            'tablePrefix' => 'pal_',
+//                    ),
+            
+                //MySQL database on MAC Server
 		'db'=>array(
-                            'connectionString' => 'mysql:host=localhost;dbname=palcineweb',
-                            'emulatePrepare' => true,
-                            'username' => 'root',
-                            'password' => '',
-                            'charset' => 'utf8',
-                            'tablePrefix' => 'pal_',
-                    ),
+			'connectionString' => 'mysql:host=127.0.0.1;port=8889;dbname=palcineweb;unix_socket:/Applications/MAMP/tmp/mysql/mysql.sock',
+                        'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => 'root',
+			'charset' => 'utf8',
+                        'tablePrefix' => 'pal_',
+		),
 		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
--- protected/migrations/m131024_214700_create_database_structure.php
+++ protected/migrations/m131024_214700_create_database_structure.php
@@ -4,27 +4,7 @@ class m131024_214700_create_database_structure extends CDbMigration
 {
 	public function up()
 	{
-		$this->createTable('pal_movie',
-			array(
-				'id'=>'pk',
-				'name'=>'VARCHAR(100) NOT NULL',
-				'name_english'=>'VARCHAR(100) NOT NULL',
-				'description'=>'VARCHAR(500) NOT NULL',
-				'release_date'=>'datetime DEFAULT NULL',
-				'length'=>'VARCHAR(100) NOT NULL',
-                                'genre_id'=>'INT(11) NOT NULL',
-				'is_premiere'=>'INT(1) NOT NULL',
-				'image'=>'VARCHAR(500) NOT NULL',
-				'image_thumbnail'=>'VARCHAR(500) NOT NULL',
-				'image_thumbnail2x'=>'VARCHAR(500) NOT NULL',
-                                'trailer_link'=>'VARCHAR(500) NOT NULL',
-				'raiting'=>'FLOAT(10,2) NOT NULL',
-				'restriction'=>'VARCHAR(50) NOT NULL',
-                                'create_time'=>'datetime DEFAULT NULL',
-				'create_user'=>'string DEFAULT NULL',
-                                'update_time'=>'datetime DEFAULT NULL',
-				'update_user'=>'string DEFAULT NULL',
-			), 'ENGINE=InnoDB');
+		
                 
                 $this->createTable('pal_banner',
 			array(
@@ -49,6 +29,120 @@ class m131024_214700_create_database_structure extends CDbMigration
 				'name'=>'VARCHAR(255) NOT NULL',
 				'dial_code'=>'VARCHAR(2) NOT NULL',
 			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_franchise',
+			array(
+				'id'=>'pk',
+				'name'=>'VARCHAR(100) NOT NULL',
+				'description'=>'VARCHAR(500) NOT NULL',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_genre',
+			array(
+				'id'=>'pk',
+				'name'=>'VARCHAR(100) NOT NULL',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_movie',
+			array(
+				'id'=>'pk',
+				'name'=>'VARCHAR(100) NOT NULL',
+				'name_english'=>'VARCHAR(100) NOT NULL',
+				'description'=>'VARCHAR(500) NOT NULL',
+				'release_date'=>'datetime DEFAULT NULL',
+				'length'=>'VARCHAR(100) NOT NULL',
+                                'genre_id'=>'INT(11) NOT NULL',
+				'is_premiere'=>'INT(1) NOT NULL',
+				'image'=>'VARCHAR(500) NOT NULL',
+				'image_thumbnail'=>'VARCHAR(500) NOT NULL',
+				'image_thumbnail2x'=>'VARCHAR(500) NOT NULL',
+                                'trailer_link'=>'VARCHAR(500) NOT NULL',
+				'raiting'=>'FLOAT(10,2) NOT NULL',
+				'restriction'=>'VARCHAR(50) NOT NULL',
+                                'create_time'=>'datetime DEFAULT NULL',
+				'create_user'=>'INT (11) DEFAULT NULL',
+                                'update_time'=>'datetime DEFAULT NULL',
+				'update_user'=>'INT (11) DEFAULT NULL',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_movie_comment',
+			array(
+				'id'=>'pk',
+                                'movie_id'=>'INT(11) NOT NULL',
+                                'user_id'=>'INT(11) NOT NULL',
+				'comment'=>'VARCHAR(500) NOT NULL',
+                                'create_time'=>'datetime DEFAULT NULL',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_movie_vote',
+			array(
+				'id'=>'pk',
+                                'movie_id'=>'INT(11) NOT NULL',
+                                'user_id'=>'INT(11) NOT NULL',
+				'vote'=>'INT(1) NOT NULL',
+                                'create_time'=>'datetime DEFAULT NULL',
+                                'UNIQUE KEY `movie_id` (`movie_id`)',
+                                'UNIQUE KEY `user_id` (`user_id`)',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_new',
+			array(
+				'id'=>'pk',
+				'title'=>'VARCHAR(100) NOT NULL',
+				'description'=>'VARCHAR(500) NOT NULL',
+				'is_published'=>'INT (1)',
+				'url'=>'VARCHAR(500) NOT NULL',
+				'image_thumbnail'=>'VARCHAR(500) NOT NULL',
+				'image_thumbnail2x'=>'VARCHAR(500) NOT NULL',
+                                'create_time'=>'datetime DEFAULT NULL',
+				'create_user'=>'INT (11) DEFAULT NULL',
+                                'update_time'=>'datetime DEFAULT NULL',
+				'update_user'=>'INT (11) DEFAULT NULL',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_page',
+			array(
+				'id'=>'pk',
+				'name'=>'VARCHAR(100) NOT NULL',
+                                'page'=>'VARCHAR(100) NOT NULL',
+                                'alias'=>'VARCHAR(100) NOT NULL',
+			), 'ENGINE=InnoDB');
+                
+                $this->createTable('pal_room',
+			array(
+				'id'=>'pk',
+				'name'=>'VARCHAR(100) NOT NULL',
+                                'theater_id'=>'INT(11) NOT NULL',
+                                'is_3d'=>'INT(1) NOT NULL',
+			), 'ENGINE=InnoDB');
+                
+                 $this->createTable('pal_room_time',
+			array(
+				'id'=>'pk',
+                                'room_id'=>'INT(11) NOT NULL',
+                                'time'=>'time DEFAULT NULL',
+                                'movie_id'=>'INT(11) NOT NULL',
+                                'UNIQUE KEY (`room_id`, `movie_id`)'
+			), 'ENGINE=InnoDB');
+                 
+                 $this->createTable('pal_movie',
+			array(
+				'id'=>'pk',
+                                'franchise_id'=>'INT(11) NOT NULL',
+				'name'=>'VARCHAR(100) NOT NULL',
+				'address'=>'VARCHAR(500) NOT NULL',
+				'latitude'=>'VARCHAR(50) NOT NULL',
+                                'longitude'=>'VARCHAR(50) NOT NULL',
+				'price'=>'VARCHAR(50) NOT NULL',
+                                'price_3d'=>'VARCHAR(50) NOT NULL',
+				'is_tuesday_half_price'=>'INT(1) NOT NULL',
+				'country_id'=>'VARCHAR(2) NOT NULL',
+                                'city_id'=>'VARCHAR(5) NOT NULL',
+                                'create_time'=>'datetime DEFAULT NULL',
+				'create_user'=>'INT (11) DEFAULT NULL',
+                                'update_time'=>'datetime DEFAULT NULL',
+				'update_user'=>'INT (11) DEFAULT NULL',
+			), 'ENGINE=InnoDB');
 	}
 
 	public function down()
@@ -56,6 +150,7 @@ class m131024_214700_create_database_structure extends CDbMigration
 		$this->dropTable('pal_movie');
                 $this->dropTable('pal_banner');
                 $this->dropTable('pal_city');
+                $this->dropTable('pal_country');
 		//echo "m130604_174032_create_question_table does not support migration down.\n";
 		//return false;
 	}
