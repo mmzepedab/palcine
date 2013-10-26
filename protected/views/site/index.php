caf3d125baa5e414d51f657d2f21cf1c8f2346c5
mmzepedab <mmzepedab@Mario-Zepedas-MacBook-Pro.local>
2013-10-26 12:44:45 -0600

    Visual changes

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,36 +4,93 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=164203760401068";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-<h1>En cartelera</h1>
+<div id="title_background">
+    Voy palCine
+</div>
+<div class="line-separator"></div>
+</br>
+<div align="center">
+    Como queres buscar tu pelicula?
+</div>
+</br>
+<div align="center">
+    <a href="<?php echo Yii::app()->createAbsoluteUrl('Issue/myAdmin'); ?>" class="blue button">Por Hora</a>
+    <a href="<?php echo Yii::app()->createAbsoluteUrl('Issue/myAdmin'); ?>" class="blue button">Por Cine</a>
+</div>
 
+
+</br>
+</br>
+
+<div id="title_background">
+    En cartelera
+</div>
+
+<div class="line-separator"></div>
+
+</br>
 
 <div id="carousel-image-and-text" class="touchcarousel grey-blue">       
 			<ul class="touchcarousel-container">
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
-					    <img src="images/170x230/2.jpg" width="170" height="230" />
-					    <h4>Lorem Ipsum</h4>    
-						<p>Congratulations! You have successfully created your Yii application. </p>
+					    <h4>El planta de los simios la rendicion del simio </h4>
+                                            
+                                            <img id="myImage" src="images/170x230/2.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars4.png" width="170" height="30"/>
+                                            </div>
+					       
+                                            </a>
+                                    <div align="center">
+                                        <a href="<?php echo Yii::app()->createAbsoluteUrl('Issue/myAdmin'); ?>" class="blue smallButton">Horarios</a>
+                                    </div>
+                                    </br>
+                                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+                                    
+                                </li>
+				<li class="touchcarousel-item">
+					<a class="item-block" href="#">
+                                            <h4>El planta de los simios la rendicion del simio </h4>
+					    <img id="myImage" src="images/170x230/3.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars3.png" width="170" height="30"/>
+                                            </div>
+					    
 					</a>
+                                    <div align="center">
+                                        <a href="<?php echo Yii::app()->createAbsoluteUrl('Issue/myAdmin'); ?>" class="blue smallButton">Horarios</a>
+                                    </div>
+                                    </br>
+                                    <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="The pixel width of the plugin" data-height="The pixel height of the plugin" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+                                    
 				</li>
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
-					    <img src="images/170x230/3.jpg" width="170" height="230" />
-					    <h4>Lorem Ipsum</h4>    
-						<p>Dolor sit amet</p>
-					</a>
-				</li>
-				<li class="touchcarousel-item">
-					<a class="item-block" href="#">
+                                            <h4>Lorem Ipsum</h4>  
 					    <img src="images/170x230/4.jpg" width="170" height="230" />
-					    <h4>Lorem Ipsum</h4>    
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars1.png" width="170" height="30"/>
+                                            </div>
+					      
 						<p>Dolor sit amet</p>
 					</a>
 				</li>
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
 					    <img src="images/170x230/5.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars3.png" width="170" height="30"/>
+                                            </div>
 					    <h4>Lorem Ipsum</h4>    
 						<p>Dolor sit amet</p>
 					</a>
@@ -41,6 +98,9 @@ $this->pageTitle=Yii::app()->name;
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
 					    <img src="images/170x230/6.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars5.png" width="170" height="30"/>
+                                            </div>
 					    <h4>Lorem Ipsum</h4>    
 						<p>Dolor sit amet</p>
 					</a>
@@ -48,6 +108,9 @@ $this->pageTitle=Yii::app()->name;
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
 					    <img src="images/170x230/3.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars1.png" width="170" height="30"/>
+                                            </div>
 					    <h4>Lorem Ipsum</h4>    
 						<p>Dolor sit amet</p>
 					</a>
@@ -55,6 +118,9 @@ $this->pageTitle=Yii::app()->name;
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
 					    <img src="images/170x230/4.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars2.png" width="170" height="30"/>
+                                            </div>
 					    <h4>Lorem Ipsum</h4>    
 						<p>Dolor sit amet</p>
 					</a>
@@ -62,6 +128,9 @@ $this->pageTitle=Yii::app()->name;
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
 					    <img src="images/170x230/5.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars4.png" width="170" height="30"/>
+                                            </div>
 					    <h4>Lorem Ipsum</h4>    
 						<p>Dolor sit amet</p>
 					</a>
@@ -69,6 +138,9 @@ $this->pageTitle=Yii::app()->name;
 				<li class="touchcarousel-item">
 					<a class="item-block" href="#">
 					    <img src="images/170x230/6.jpg" width="170" height="230" />
+                                            <div id="stars">
+                                                <img class="my-item-block"src="images/stars1.png" width="170" height="30"/>
+                                            </div>
 					    <h4>Lorem Ipsum</h4>    
 						<p>Dolor sit amet</p>
 					</a>
@@ -101,13 +173,15 @@ jQuery(function($) {
     $("#carousel-image-and-text").touchCarousel({
         /* carousel options go here see Javascript Options section for more info */
         				
-				pagingNav: false,
+				pagingNav: true,
 				snapToItems: false,
-				itemsPerMove: 4,				
-				scrollToLast: false,
-				loopItems: false,
-				scrollbar: true
-		    
+				itemsPerMove: 1,				
+				scrollToLast: true,
+				loopItems: true,
+				scrollbar: true,
+                                autoplay:true,               // Autoplay enabled.
+                                autoplayDelay:2000,	          // Delay between transitions.
+                                autoplayStopAtAction:true,
 
     });
 });
